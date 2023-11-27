7748b4b1b9c21a74e3d94d65cf8a56c16ece2021
Ariel Oak <codestothestars@yahoo.com>
2023-11-26 19:03:35 -0500

    Improve queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- Initial delay for spell for creature.
--- Includes time alive in case the creature died without casting, which may increase the max delay.
+-- Includes time alive as a minimum ceiling.
 SELECT
   ROUND(
     (CAST(spell_initial_cast.unixtimems AS SIGNED) - CAST(creature_combat_start.unixtimems AS SIGNED)) / 1000
@@ -19,37 +19,60 @@ WHERE
   creature.id = 12557;
 
 -- Repeat delay for spell for creature.
-SELECT ROUND(AVG(current_cast.unixtimems - previous_cast.unixtimems) / 1000)
-FROM spell_cast_start current_cast
-  JOIN spell_cast_start previous_cast
-    ON current_cast.caster_guid = previous_cast.caster_guid AND current_cast.spell_id = previous_cast.spell_id
-  JOIN creature
-    ON current_cast.caster_guid = creature.guid
+-- Includes time left alive as a minimum ceiling where the creature never repeated the spell.
+SELECT
+  creature.guid,
+  ROUND((current_cast.unixtimems - previous_cast.unixtimems) / 1000) spell_repeat_cast_delay,
+  ROUND((creature_death.unixtimems - current_cast.unixtimems) / 1000) creature_time_left_alive
+FROM creature
+  JOIN spell_cast_start current_cast
+    ON creature.guid = current_cast.caster_guid
+  LEFT JOIN spell_cast_start previous_cast
+    ON current_cast.caster_guid = previous_cast.caster_guid
+    AND current_cast.spell_id = previous_cast.spell_id
+    AND current_cast.unixtimems > previous_cast.unixtimems
   LEFT JOIN spell_cast_start between_cast
     ON current_cast.caster_guid = between_cast.caster_guid
     AND current_cast.spell_id = between_cast.spell_id
     AND current_cast.unixtimems > between_cast.unixtimems
-    AND previous_cast.unixtimems < between_cast.unixtimems 
+    AND previous_cast.unixtimems < between_cast.unixtimems
+  LEFT JOIN (
+    SELECT MAX(unixtimems) unixtimems, guid FROM creature_values_update WHERE current_health = 0 GROUP BY guid
+  ) creature_death ON creature.guid = creature_death.guid 
 WHERE
   between_cast.unixtimems IS NULL
   AND creature.id = 12557
   AND current_cast.spell_id = 22274
-  AND current_cast.unixtimems > previous_cast.unixtimems;
+ORDER BY creature.guid, current_cast.unixtimems;
 
 -- Distinct spells cast by creature.
 SELECT spell_id FROM (
-  SELECT spell_id, caster_id FROM spell_cast_failed
+  SELECT caster_id, spell_id FROM spell_cast_failed
   UNION
-  SELECT spell_id, caster_id FROM spell_cast_go
+  SELECT caster_id, spell_id FROM spell_cast_go
   UNION
-  SELECT spell_id, caster_id FROM spell_cast_start
+  SELECT caster_id, spell_id FROM spell_cast_start
   UNION
-  SELECT spell_id, caster_id FROM spell_channel_start
+  SELECT caster_id, spell_id FROM spell_channel_start
   UNION
-  SELECT spell_id, caster_id FROM spell_unique_caster
-) spell
+  SELECT caster_id, spell_id FROM spell_unique_caster
+) creature_spell
 WHERE caster_id = 12557
 ORDER BY spell_id;
+
+-- Count of distinct spells cast by creature.
+SELECT COUNT(DISTINCT spell_id) FROM (
+  SELECT caster_id, spell_id FROM spell_cast_failed
+  UNION
+  SELECT caster_id, spell_id FROM spell_cast_go
+  UNION
+  SELECT caster_id, spell_id FROM spell_cast_start
+  UNION
+  SELECT caster_id, spell_id FROM spell_channel_start
+  UNION
+  SELECT caster_id, spell_id FROM spell_unique_caster
+) creature_spell
+WHERE caster_id = 12557;
 
 -- Delay of creature's each movement from the previous.
 SELECT current.unixtimems, current.point, current.orientation, ROUND((current.unixtimems - previous.unixtimems) / 1000) delay
